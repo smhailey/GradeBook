4ca9df94c2f80d6aca65f397bfc10190320a81a9
Sean Hailey <sean.hailey@intimetec.com>
2019-11-19 14:33:38 -0700

    Following along from Pluralsight course

---
+++ GradeBook.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            //arrange
+            var.book = new Book("");
+
+        }
+    }
+}
