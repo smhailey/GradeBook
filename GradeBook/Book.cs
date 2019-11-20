ac61db9315e4ac1427b1d31f9c779c0e81a15d11
Sean Hailey <sean.hailey@intimetec.com>
2019-11-20 09:20:16 -0700

    Added BookTests

--- GradeBook/Book.cs
+++ GradeBook/Book.cs
@@ -1,9 +1,45 @@
-﻿namespace GradeBook
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
 {
-    internal class Book
+    public class Book
     {
-        public Book()
+        // explicit constructor
+        public Book(string name)
         {
+            grades = new List<double>();
+            this.name = name;
         }
+
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach (var number in grades)
+            {
+                //if (number > lowGrade)
+                //{
+                //    lowGrade = number;
+                //}
+                lowGrade = Math.Min(number, lowGrade);
+                highGrade = Math.Max(number, highGrade);
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The highest grade is {highGrade}");
+            Console.WriteLine($"The average grade is {result:N1}");
+        }
+
+        private List<double> grades;
+        private string name;
     }
 }
